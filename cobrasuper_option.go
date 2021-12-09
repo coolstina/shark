b437993efe761aac4417ff5a9e2980922f07818f
helloshaohua <wu.shaohua@foxmail.com>
2021-12-09 21:55:08 +0800

    fix: fixed reconsitution modified package name

--- cobrasuper_option.go
+++ cobrasuper_option.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cobrax
+package cobrasuper
 
 type Option interface {
 	apply(*options)
